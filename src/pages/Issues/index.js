4cc7f520729b1832b57fbc62cc74692cc98f0420
Caio Rodrigues <caiorodrigues@hotmail.com>
2019-05-30 18:40:24 -0300

    GoRN - Challenge2 - Structuring Issues

--- src/pages/Issues/index.js
+++ src/pages/Issues/index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 
@@ -9,8 +17,43 @@ export default class Issues extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Issues</Text>
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <View style={styles.container}>
+          <TouchableOpacity onPress={() => { }}>
+            <Text>Todas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <Text>Abertas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <Text>Fechadas</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity onPress={() => { }}>
+          <Image source={''} />
+          <View>
+            <Text>Título</Text>
+            <Text>Usuário da issue</Text>
+          </View>
+          <Icon style={styles.icon} name="star" size={16} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { }}>
+          <Image source={''} />
+          <View>
+            <Text>Título</Text>
+            <Text>Usuário da issue</Text>
+          </View>
+          <Icon style={styles.icon} name="star" size={16} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { }}>
+          <Image source={''} />
+          <View>
+            <Text>Título</Text>
+            <Text>Usuário da issue</Text>
+          </View>
+          <Icon style={styles.icon} name="star" size={16} />
+        </TouchableOpacity>
       </View>
     );
   };
